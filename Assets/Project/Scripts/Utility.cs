132f1c68338c6ca5aaaaf84be8e01aca1f7ce58b
ASUS <dtawan.arunsawad@gmail.com>
2024-06-05 17:18:50 +0700

    Add Utility SetField

---
+++ Assets/Project/Scripts/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project.Scripts
+{
+    public static class Utility
+    {
+        public static void SetField<T>(ref T field, T value, object sender,
+            Action<object, PropertyChangedEventArgs> eventAction, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return;
+            field = value;
+            eventAction?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
